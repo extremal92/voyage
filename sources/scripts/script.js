ba041f3e8216c5d6a1c8a9d196ca6090dfea1758
eXtReMaL1992 <Romyk200492@mail.ru>
2018-02-01 16:18:02 +0200

    add images

---
+++ sources/scripts/script.js
@@ -0,0 +1,22 @@
+// var user = {
+//     name: "Frank",
+//     id: 13232,
+//     lastVisit: Date.now(),
+//     friends: [2424, 5252, 4512],
+//     toJSON: function () {
+//         return{
+//             name: this.name,
+//             lastVisit: this.lastVisit,
+//         }
+//     }
+// };
+//
+// var userData = JSON.stringify(user);
+// console.log(userData);
+//
+// console.log(JSON.parse(userData));
+
+
+// var date = new Date();
+// var date = new Date(1992, 8, 6, 04, 20, 00);
+// console.log(date);
